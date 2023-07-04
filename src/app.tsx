96d5c7ce529f0c376100e2800fb65b93e5076cc0
Hiep Nguyen <nguyenhieptech@gmail.com>
2023-07-04 14:05:16 +0700

    feat: init project

---
+++ src/app.tsx
@@ -0,0 +1,14 @@
+import { Button } from '@/components';
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold">
+        Hello React Admin Dashboard
+        <Button />
+      </h1>
+    </>
+  );
+}
+
+export default App;
